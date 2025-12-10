0277f323de9cedc411094e6c4437ed5cd9c3a7f1
MM2-ScriptsZ <stfuhis1@gmail.com>
2025-12-10 20:51:08 +0800

    Update legit.lua

--- legit.lua
+++ legit.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8691bc2f38c0cbd4689fdf708e1723d932e10c2614507a70092a688b4e1ff033/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/a281ea7ce74827a087cde9cdf5bb7d7c3286c375e0b30deb3726496bec44778a/download"))()
